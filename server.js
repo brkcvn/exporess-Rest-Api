d8bcf829e65eed12eaedf9c10abb004d6ad695b4
Burak Civan <burakcivan22@gmail.com>
2021-01-13 16:31:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
+
+//converts data to json format.
 var bodyParser = require('body-parser');
 
+//sample datas
 var cities = [{ name: 'Istanbul', country: 'Turkey' }, { name: 'Berlin', country: 'Germany' }];
 
 var app = express();
@@ -8,13 +11,16 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//Added port name
 app.listen(3500, function () {
 });
 
+//use get method
 app.get('/api/cities', function (request, response) {
   response.send(cities);
 });
 
+//use post method
 app.post('/api/cities', function (request, response) {
   var city = request.body;
   for (var index = 0; index < cities.length; index++) {
